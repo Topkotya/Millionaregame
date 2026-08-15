2901e09d5af47d91e0909944149a4b3102781ff1
KOt <nkotanov1234@mail.ru>
2026-08-15 15:00:07 +0300

    v1.01
    upd

---
+++ WpfApp2/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _statusText = "Нажмите кнопку";
+        public string StatusText
+        {
+            get => _statusText;
+            set
+            {
+                _statusText = value;
+                OnPropertyChanged(nameof(StatusText));
+            }
+        }
+        public ICommand SubmitCommand { get; }
+
+        public MainViewModel()
+        {
+            // Передаем метод выполнения и (опционально) условие активности кнопки
+            SubmitCommand = new RelayCommand(OnSubmit, CanSubmit);
+        }
+
+        // Логика, которая выполнится при нажатии
+        private void OnSubmit(object parameter)
+        {
+            StatusText = $"Кнопка нажата! Параметр: {parameter ?? "нет"}";
+        }
+
+        // Логика проверки: должна ли кнопка быть активна (IsEnable)
+        private bool CanSubmit(object parameter)
+        {
+            return true; // Верните false, чтобы временно отключить кнопку
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -20,5 +20,25 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        //private void Button_Click_2(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        //private void Button_Click_3(object sender, RoutedEventArgs e)
+        //{
+
+        //}
     }
 }
