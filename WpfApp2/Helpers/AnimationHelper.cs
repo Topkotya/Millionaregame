2a0ce1cea36fd1c07d9d8a4539cd49d725177877
KOt <nkotanov1234@mail.ru>
2026-09-03 15:09:26 +0500

    v1.0.3
    v1.0.3

---
+++ WpfApp2/Helpers/AnimationHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace WpfApp2
+{
+    public class AnimationHelper
+    {
+        private Color LerpColor(Color start, Color end, double t)
+        {
+            byte a = (byte)(start.A + (end.A - start.A) * t);
+            byte r = (byte)(start.R + (end.R - start.R) * t);
+            byte g = (byte)(start.G + (end.G - start.G) * t);
+            byte b = (byte)(start.B + (end.B - start.B) * t);
+            return Color.FromArgb(a, r, g, b);
+        }
+        public async Task PlayButtonAnimation(Action<Brush> brushCallBack, bool isCorrect)
+        {
+            double t = 0;
+            if (isCorrect) //анимация верного ответа
+            {
+                Color start = Color.FromRgb(11, 47, 120); //переход из тёмно синего в светло зелёный
+                Color end = Color.FromRgb(18, 200, 0);
+                while (t < 1)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t += 0.03;
+                    await Task.Delay(15);
+                }
+
+                t = 0;
+                start = end;
+                end = Color.FromRgb(0, 140, 9); //моргание оттенков зелёного
+                while (t < 1)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t += 0.03;
+                    await Task.Delay(27);
+                }
+                while (t > 0)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t -= 0.03;
+                    await Task.Delay(17);
+                }
+                while (t < 1)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t += 0.03;
+                    await Task.Delay(22);
+                }
+                while (t > 0)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t -= 0.03;
+                    await Task.Delay(12);
+                }
+            }
+
+            else
+            {
+                Color start = Color.FromRgb(11, 47, 120);
+                Color end = Color.FromRgb(255, 0, 0);
+                while (t < 1)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t += 0.03;
+                    await Task.Delay(15);
+                }
+
+                t = 0;
+                start = end;
+                end = Color.FromRgb(140, 0, 0);
+                while (t < 1)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t += 0.03;
+                    await Task.Delay(27);
+                }
+                while (t > 0)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t -= 0.03;
+                    await Task.Delay(17);
+                }
+                while (t < 1)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t += 0.03;
+                    await Task.Delay(22);
+                }
+                while (t > 0)
+                {
+                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
+                    t -= 0.03;
+                    await Task.Delay(12);
+                }
+            }
+        }
+
+    }
+}
--- WpfApp2/ViewModel/MainViewModel.cs
+++ WpfApp2/ViewModel/MainViewModel.cs
@@ -15,6 +15,7 @@ namespace WpfApp2
     public class MainViewModel : INotifyPropertyChanged
     {
         private MainModel mod = new MainModel();
+        private AnimationHelper animator = new AnimationHelper();
         private const int WinningRound = 11;
         private bool isAnswering;
         public bool WinScreenState
@@ -150,105 +151,13 @@ namespace WpfApp2
             PrintRound = "1";
             FieldAnswers();
             SubmitCommand = new RelayCommand(OnSubmit, CanSubmit);
-        }
-        private Color LerpColor(Color start, Color end, double t)
-        {
-            byte a = (byte)(start.A + (end.A - start.A) * t);
-            byte r = (byte)(start.R + (end.R - start.R) * t);
-            byte g = (byte)(start.G + (end.G - start.G) * t);
-            byte b = (byte)(start.B + (end.B - start.B) * t);            
-            return Color.FromArgb(a, r, g, b);
-        }
-        private async Task PlayButtonAnimation(Action<Brush> brushCallBack, bool isCorrect)
-        {
-            double t = 0;
-            if (isCorrect) //анимация верного ответа
-            {
-                Color start = Color.FromRgb(11, 47, 120); //переход из тёмно синего в светло зелёный
-                Color end = Color.FromRgb(18, 200, 0);
-                while (t < 1) 
-                {                    
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t += 0.03;
-                    await Task.Delay(15);
-                }
-
-                t = 0;
-                start = end;
-                end = Color.FromRgb(0, 140, 9); //моргание оттенков зелёного
-                while (t < 1)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t += 0.03;
-                    await Task.Delay(27);
-                }
-                while (t > 0)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t -= 0.03;
-                    await Task.Delay(17);
-                }
-                while (t < 1)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t += 0.03;
-                    await Task.Delay(22);
-                }
-                while (t > 0)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t -= 0.03;
-                    await Task.Delay(12);
-                }
-            }
-
-            else
-            {
-                Color start = Color.FromRgb(11, 47, 120); 
-                Color end = Color.FromRgb(255, 0, 0);
-                while (t < 1)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t += 0.03;
-                    await Task.Delay(15);
-                }
-
-                t = 0;
-                start = end;
-                end = Color.FromRgb(140, 0, 0); 
-                while (t < 1)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t += 0.03;
-                    await Task.Delay(27);
-                }
-                while (t > 0)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t -= 0.03;
-                    await Task.Delay(17);
-                }
-                while (t < 1)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t += 0.03;
-                    await Task.Delay(22);
-                }
-                while (t > 0)
-                {
-                    brushCallBack(new SolidColorBrush(LerpColor(start, end, t)));
-                    t -= 0.03;
-                    await Task.Delay(12);
-                }
-            }
-        }
+        }        
         private async Task SetAnswerColor(string answer, bool isCorrect)
         {            
-            //Brush color = isCorrect ? Brushes.Green : Brushes.Red;
-            if (answer == Answer1) await PlayButtonAnimation(b => Answer1Background = b, isCorrect);
-            else if (answer == Answer2) await PlayButtonAnimation(b => Answer2Background = b, isCorrect);
-            else if (answer == Answer3) await PlayButtonAnimation(b => Answer3Background = b, isCorrect);
-            else if (answer == Answer4) await PlayButtonAnimation(b => Answer4Background = b, isCorrect);
+            if (answer == Answer1) await animator.PlayButtonAnimation(b => Answer1Background = b, isCorrect);
+            else if (answer == Answer2) await animator.PlayButtonAnimation(b => Answer2Background = b, isCorrect);
+            else if (answer == Answer3) await animator.PlayButtonAnimation(b => Answer3Background = b, isCorrect);
+            else if (answer == Answer4) await animator.PlayButtonAnimation(b => Answer4Background = b, isCorrect);
         }
         private async void OnSubmit(object parameter)
         {
@@ -265,10 +174,10 @@ namespace WpfApp2
                 if (int.Parse(mod.CurrentRound) == WinningRound)
                 {
                     WinScreenState = true;
-                    //MessageBox.Show("Вы стали миллионером! Вы победили!","Игра окончена",MessageBoxButton.OK,MessageBoxImage.Information);                    
                     return;
                 }
                 PrintRound = mod.CurrentRound;
+
                 // Возвращаем кнопкам исходный градиент
                 Answer1Background = null;
                 Answer2Background = null;
@@ -279,7 +188,6 @@ namespace WpfApp2
             else
             {
                 LoseScreenState = true;
-                //MessageBox.Show("Вы проиграли", "Игра окончена", MessageBoxButton.OK, MessageBoxImage.Error);                
                 return;
             }
             isAnswering = false;
